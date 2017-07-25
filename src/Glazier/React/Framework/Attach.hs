81787c78b94e0c1b7456ddb7fc31eb1746991108
Louis Pan <louis@lory.local>
2017-07-25 22:35:34 +1000

    commented on Attach

--- src/Glazier/React/Framework/Attach.hs
+++ src/Glazier/React/Framework/Attach.hs
@@ -9,7 +9,11 @@ import Data.Diverse
 import Data.Proxy
 
 class Attach a b c where
+    -- | Combine two things together in a '<>' Semigroup sense at both the value and type level (so the type also changes).
+    -- 'aempty' is an identity with respects to '+<>+'
     (+<>+) :: a -> b -> c
+    -- | Combine two things together in a '<|>' Semigroup sense at both the value and type level (so the type also changes).
+    -- 'aempty' is an identity with respects to '+<|>+'
     (+<|>+) :: a -> b -> c
 
 infixr 6 +<>+ -- like <>
@@ -20,6 +24,7 @@ instance c ~ Append a b => Attach (Proxy a) (Proxy b) (Proxy c) where
     (+<|>+) = (+<>+)
 
 class AttachId a where
+    -- | The Identity for 'Attach'
     aempty :: a
 
 instance AttachId (Proxy '[]) where

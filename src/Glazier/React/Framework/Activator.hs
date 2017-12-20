1097b3bcbbcb25dc92cccb21504b8286388d5aa1
Louis Pan <louis@pan.me>
2017-12-20 23:27:10 +1100

    trying an Executor that creates (Handler, Activator)

--- src/Glazier/React/Framework/Activator.hs
+++ src/Glazier/React/Framework/Activator.hs
@@ -8,101 +8,105 @@
 
 module Glazier.React.Framework.Activator where
 
-import Control.DeepSeq
+-- import Control.DeepSeq
 import Control.Lens
-import Data.Diverse.Lens
-import qualified Data.DList as DL
-import Data.Foldable
+-- import Data.Diverse.Lens
+-- import qualified Data.DList as DL
+-- import Data.Foldable
 import Data.IORef
 import Data.Semigroup
-import qualified Glazier.React as R
+-- import qualified Glazier.React as R
 import qualified Glazier.React.Framework.Core as F
-import qualified Glazier.React.Framework.Executor as F
-import qualified Glazier.React.Framework.Handler as F
-import qualified Glazier.React.Framework.Trigger as F
-import qualified Parameterized.Data.Monoid as P
+-- import qualified Glazier.React.Framework.Executor as F
+-- import qualified Glazier.React.Framework.Handler as F
+-- import qualified Glazier.React.Framework.Trigger as F
+-- import qualified Parameterized.Data.Monoid as P
 
 ------------------------------------------
 
-newtype Activator m r x c = Activator
-    { runActivator :: F.Executor m c (DL.DList x) -- final transformation)
-                   -> r -- Handler env
+newtype Activator m r = Activator
+    { runActivator :: r -- Handler env
                    -> m () -- return the monadic action to commit the activation
     }
 
 -- | Uses ReifiedLens' to avoid impredicative polymorphism
-type RefActivator m v s x c = Activator m (IORef v, ReifiedLens' v s) x c
+type RefActivator m v s = Activator m (IORef v, ReifiedLens' v s)
 
-instance Monad m => Semigroup (Activator m r x c) where
-    (Activator f) <> (Activator g) = Activator $ \exec env ->
-        f exec env >>
-        g exec env
+instance Monad m => Semigroup (Activator m r) where
+    (Activator f) <> (Activator g) = Activator $ \env ->
+        f env >>
+        g env
 
-instance Monad m => Monoid (Activator m r x c) where
-    mempty = Activator $ \_ _ -> pure ()
+instance Monad m => Monoid (Activator m r) where
+    mempty = Activator $ \_ -> pure ()
     mappend = (<>)
 
-------------------------------------------
+instance Contravariant (Activator m) where
+    contramap f (Activator act) = Activator $ act . f
 
-type instance P.PNullary (Activator m r x) c = Activator m r x c
+-- ------------------------------------------
 
-instance Applicative m => P.PMEmpty (Activator m r x) (Which '[]) where
-    pmempty = Activator $ \_ _ -> pure ()
+-- type instance P.PNullary (Activator m r x) c = Activator m r x c
 
-instance ( Monad m
-         , Reinterpret' c3 c1
-         , Reinterpret' c3 c2
-         , c3 ~ AppendUnique c1 c2
-         ) => P.PSemigroup (Activator m r x) (Which c1) (Which c2) (Which c3) where
-    (Activator f) `pmappend` (Activator g) = Activator $ \exec env ->
-        f (F.suppressExecutor reinterpret' exec) env >>
-        g (F.suppressExecutor reinterpret' exec) env
+-- instance Applicative m => P.PMEmpty (Activator m r x) (Which '[]) where
+--     pmempty = Activator $ \_ _ -> pure ()
 
-------------------------------------------
-
--- | Ignore certain commands
-suppressActivator :: (c -> Maybe c') ->  Activator m r x c -> Activator m r x c'
-suppressActivator f (Activator act) = Activator $ \exec env -> act (F.suppressExecutor f exec) env
-
--- | Map a function to the commands accepted by the Executor
-instance Functor m => Functor (Activator m r x) where
-    fmap f (Activator act) = Activator $ \exec env -> act (F.contramapExecutor f exec) env
-
--- | Create callbacks from triggers and add it to this state's dlist of listeners.
-triggersRefActivator
-    :: (R.MonadReactor x m, NFData a)
-    => [F.Trigger a]
-    -> RefActivator m v (DL.DList R.Listener) x a
-triggersRefActivator triggers = Activator $ \(F.Executor exec) (ref, Lens this) -> do
-    cbs <- traverse {- list of triggers traverse -}
-              (traverse {- tuple traverse -} (\t' ->
-                   R.mkCallback t' exec) . F.runTrigger) triggers
-    R.doModifyIORef' ref (this %~ (`DL.append` DL.fromList cbs))
-  where
-    -- go :: (a -> m (DL.DList x)) -> DL.DList a -> m (DL.DList x)
-    -- go exec as = fold <$> traverse exec (DL.toList as)
-
--- | Variation of 'triggersRefActivator' using a state of @(F.ComponentModel, Many s)@
-triggersRefActivator'
-    :: (R.MonadReactor x m, NFData a, HasItem' (DL.DList R.Listener) s)
-    => [F.Trigger a]
-    -> RefActivator m v (F.ComponentModel, s) x a
-triggersRefActivator' = F.viaModel (_2.item') . triggersRefActivator
-
--- | Add a handler so that it is piped before the input 'Executor' in an 'Activator'
-addHandler ::
-    ( R.MonadReactor x m
-    )
-    => F.Handler m r a b -> Activator m r x a -> Activator m r x b
-addHandler (F.Handler hdl) (Activator act) = Activator $ \(F.Executor exec) env ->
-    act (F.Executor $ \as -> fold <$> traverse (hdl env) (DL.toList as) >>= exec) env
+-- instance ( Monad m
+--          , Reinterpret' c3 c1
+--          , Reinterpret' c3 c2
+--          , c3 ~ AppendUnique c1 c2
+--          ) => P.PSemigroup (Activator m r x) (Which c1) (Which c2) (Which c3) where
+--     (Activator f) `pmappend` (Activator g) = Activator $ \exec env ->
+--         f (F.suppressExecutor reinterpret' exec) env >>
+--         g (F.suppressExecutor reinterpret' exec) env
 
 ------------------------------------------
 
-newtype ActivatorModeller m v x c s = ActivatorModeller { runActivatorModeller :: RefActivator m v s x c }
+-- -- | Ignore certain commands
+-- suppressActivator :: (c -> Maybe c') ->  Activator m r x c -> Activator m r x c'
+-- suppressActivator f (Activator act) = Activator $ \exec env -> act (F.suppressExecutor f exec) env
 
-type instance F.Modeller (ActivatorModeller m v x c) s = RefActivator m v s x c
+-- -- | Map a function to the commands accepted by the Executor
+-- instance Functor m => Functor (Activator m r x) where
+--     fmap f (Activator act) = Activator $ \exec env -> act (F.contramapExecutor f exec) env
 
-instance F.ViaModel (ActivatorModeller m v a b) where
-    viaModel l (Activator f) = Activator $ \exec (ref, Lens this) ->
-        f exec (ref, Lens (this.l))
+-- -- | Create callbacks from triggers and add it to this state's dlist of listeners.
+-- triggersRefActivator
+--     :: (R.MonadReactor x m, NFData a)
+--     => [F.Trigger a]
+--     -> RefActivator m v (DL.DList R.Listener) x a
+-- triggersRefActivator triggers = Activator $ \(F.Executor exec) (ref, Lens this) -> do
+--     cbs <- traverse {- list of triggers traverse -}
+--               (traverse {- tuple traverse -} (\t' ->
+--                    R.mkCallback t' exec) . F.runTrigger) triggers
+--     R.doModifyIORef' ref (this %~ (`DL.append` DL.fromList cbs))
+--   where
+--     -- go :: (a -> m (DL.DList x)) -> DL.DList a -> m (DL.DList x)
+--     -- go exec as = fold <$> traverse exec (DL.toList as)
+
+-- -- | Variation of 'triggersRefActivator' using a state of @(F.ComponentModel, Many s)@
+-- triggersRefActivator'
+--     :: (R.MonadReactor x m, NFData a, HasItem' (DL.DList R.Listener) s)
+--     => [F.Trigger a]
+--     -> RefActivator m v (F.ComponentModel, s) x a
+-- triggersRefActivator' = F.viaModel (_2.item') . triggersRefActivator
+
+-- -- | Add a handler so that it is piped before the input 'Executor' in an 'Activator'
+-- addHandler ::
+--     ( R.MonadReactor x m
+--     )
+--     => F.Handler m r x c a b -> Activator m r x a -> Activator m r x b
+-- addHandler (F.Handler hdl) (Activator act) = Activator $ \(F.Executor exec) env ->
+--     act (F.Executor $ \as -> fold <$> traverse (hdl env) (DL.toList as) >>= exec) env
+
+-- FIXME: c needs to be a subset of b!
+
+------------------------------------------
+
+newtype ActivatorModeller m v s = ActivatorModeller { runActivatorModeller :: RefActivator m v s}
+
+type instance F.Modeller (ActivatorModeller m v) s = RefActivator m v s
+
+instance F.ViaModel (ActivatorModeller m v) where
+    viaModel l (Activator f) = Activator $ \(ref, Lens this) ->
+        f (ref, Lens (this.l))
--- src/Glazier/React/Framework/Executor.hs
+++ src/Glazier/React/Framework/Executor.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE RankNTypes #-}
@@ -14,97 +12,121 @@
 
 module Glazier.React.Framework.Executor where
 
-import Data.Diverse
+import Control.DeepSeq
+import Control.Lens
+import Data.Diverse.Lens
+import Data.Diverse.Profunctor
 import qualified Data.DList as DL
-import Data.Profunctor
-import qualified Parameterized.Control.Monad as P
+import Data.Foldable
+import Data.IORef
+import Data.Semigroup
+import qualified Glazier.React as R
+import qualified Glazier.React.Framework.Activator as F
+import qualified Glazier.React.Framework.Core as F
+import qualified Glazier.React.Framework.Handler as F
+import qualified Glazier.React.Framework.Trigger as F
+import qualified Parameterized.Data.Monoid as P
+import qualified Parameterized.TypeLevel as P
 
-newtype Executor m c x = Executor { runExecutor :: DL.DList c -> m x }
-    deriving Functor
+newtype Executor m r x c a b = Executor {
+    runExecutor
+        :: r
+        -> (DL.DList c -> m (DL.DList x)) -- final transformation
+        -> (F.Handler m r a b, F.Activator m r)
+    }
 
-instance Applicative m => Applicative (Executor m c) where
-    pure = Executor . const . pure
-    (Executor f) <*> (Executor g) = Executor $ \s -> f s <*> g s
-
-instance Monad m => Monad (Executor m c) where
-    (Executor f) >>= k = Executor $ \s -> f s >>= k' s
-      where
-        k' s x = runExecutor (k x) s
-
-instance Functor m => Profunctor (Executor m) where
-    dimap f g (Executor exec) = Executor $ \xs -> g <$> exec (f <$> xs)
+-- | Uses ReifiedLens' to avoid impredicative polymorphism
+type RefExecutor m v s x c a b = Executor m (IORef v, ReifiedLens' v s) x c a b
 
 -----------------------------------
 
-type instance P.PUnary (Executor m) c = Executor m c
+newtype PExecutor m r x cab = PExecutor
+    { runPExecutor :: Executor m r x (P.At0 cab) (P.At1 cab) (P.At2 cab)
+    }
 
-instance Applicative m => P.PPointed (Executor m) (Which '[]) where
-    ppure = pure
+type instance P.PNullary (PExecutor m r x) (c, a, b) = Executor m r x c a b
 
--- | UndecidableInstanced!
--- The intention of this combinator is to allow combining executors for different
--- input actions together.
--- Therefore, it is will be compile error to `orExecutor` of the with the same input types.
--- This is to prevent accidently processing an action twice.
--- The compile error will be due to @(Append c1 c2)@ which will not satisfy
--- @UniqueMember@ constraints.
--- NB. The use of <|> only the first executor for a particular action will be used.
-instance ( Applicative m
-         , Reinterpret' b c
-         , Reinterpret' a c
-         , (Complement c b) ~ a
-         , (Complement c a) ~ b
-         , c ~ Append a b
-         ) =>
-         P.PApplicative (Executor m) (Which a) (Which b) (Which c) where
-    papply (Executor x) (Executor y) =
-        Executor $ \cs ->
-            let lcs = foldMap lgo cs
-                rcs = foldMap rgo cs
-            in x lcs <*> y rcs
-      where
-        lgo c =
-            case reinterpret' c of
-                Nothing -> DL.empty
-                Just c' -> DL.singleton c'
-        rgo c =
-            case reinterpret' c of
-                Nothing -> DL.empty
-                Just c' -> DL.singleton c'
+instance Monad m => P.PMEmpty (PExecutor m r x) (Which '[], Which '[], Which '[]) where
+    pmempty = Executor $ \_ _ -> (P.pmempty, mempty)
 
--- | UndecidableInstanced!
+-- | Undecidableinstances!
 instance ( Monad m
-         , Reinterpret' b c
-         , Reinterpret' a c
-         , a ~ Complement c b
-         , b ~ Complement c a
-         , c ~ Append a b
+         , a3 ~ Append a1 a2
+         , b3 ~ AppendUnique b1 b2
+         , Reinterpret a2 a3
+         , a1 ~ Complement a3 a2
+         , Diversify b1 b3
+         , Diversify b2 b3
+         , c3 ~ AppendUnique c1 c2
+         , Diversify c1 c3
+         , Diversify c2 c3
          ) =>
-         P.PMonad (Executor m) (Which a) (Which b) (Which c) where
-    pbind (Executor x) k =
-        Executor $ \cs ->
-            let lcs = foldMap lgo cs
-                rcs = foldMap rgo cs
-            in x lcs >>= k' rcs
-      where
-        k' s a = runExecutor (k a) s
-        lgo c =
-            case reinterpret' c of
-                Nothing -> DL.empty
-                Just c' -> DL.singleton c'
-        rgo c =
-            case reinterpret' c of
-                Nothing -> DL.empty
-                Just c' -> DL.singleton c'
+         P.PSemigroup (PExecutor m r x)
+              (Which c1, Which a1, Which b1)
+              (Which c2, Which a2, Which b2)
+              (Which c3, Which a3, Which b3) where
+    x `pmappend` y = Executor $ \env k ->
+        let (hdl1, act1) = runExecutor (mapExecutor diversify x) env k
+            (hdl2, act2) = runExecutor (mapExecutor diversify y) env k
+        in (hdl1 +||+ hdl2, act1 <> act2)
 
--- | Ignore certain commands contravariantly
-suppressExecutor :: (c -> Maybe c') -> Executor m c' x  -> Executor m c x
-suppressExecutor f (Executor exec) = Executor $ \xs -> exec (foldMap go xs)
+-- | Ignore certain commands
+suppressExecutor :: (c -> Maybe c') -> Executor m r x c a b  -> Executor m r x c' a b
+suppressExecutor f (Executor exec) = Executor $ \env k -> exec env (k . foldMap go)
   where
     go x = case f x of
         Nothing -> mempty
         Just x' -> DL.singleton x'
 
--- | Map a function to the commands contravariantly
-contramapExecutor :: Functor m => (c -> c') -> Executor m c' x -> Executor m c x
-contramapExecutor = lmap
+-- | Map a function to the commands
+mapExecutor :: (c -> c') -> Executor m r x c a b -> Executor m r x c' a b
+mapExecutor f (Executor exec) = Executor $ \env k -> exec env (k . fmap f)
+
+-- | Create callbacks from triggers and add it to this state's dlist of listeners.
+addTriggers
+    :: (R.MonadReactor x m, NFData a)
+    => [F.Trigger a]
+    -> RefExecutor m v (DL.DList R.Listener) x a (Which '[]) (Which '[])
+addTriggers triggers = Executor $ \_ k -> (P.pmempty, F.Activator $ act k)
+  where
+    act k (ref, Lens this) = do
+        cbs <- traverse {- list of triggers traverse -}
+                  (traverse {- tuple traverse -} (\t' ->
+                       R.mkCallback t' k) . F.runTrigger) triggers
+        R.doModifyIORef' ref (this %~ (`DL.append` DL.fromList cbs))
+
+-- | Variation of 'triggersRefActivator' using a state of @(F.ComponentModel, Many s)@
+addTriggers'
+    :: (R.MonadReactor x m, NFData a, HasItem' (DL.DList R.Listener) s)
+    => [F.Trigger a]
+    -> RefExecutor m v (F.ComponentModel, s) x a (Which '[]) (Which '[])
+addTriggers' = F.viaModel (_2.item') . addTriggers
+
+-- | Add a handler so that it is piped before the final transformation to @x@
+addHandler ::
+    ( R.MonadReactor x m
+    )
+    => F.Handler m r c d -> Executor m r x c a b -> Executor m r x d a b
+addHandler (F.Handler hdl) (Executor exec) = Executor $ \env k ->
+    let (hdl', act') = exec env (k' env k)
+    in (hdl', act')
+  where
+    k' env k cs = fold <$> traverse (hdl env) (DL.toList cs) >>= k
+
+mapExecutorHandler :: (F.Handler m r a b -> F.Handler m r a' b') -> Executor m r x c a b -> Executor m r x c a' b'
+mapExecutorHandler f (Executor exec) = Executor $ \env k ->
+    let (hdl', act') = exec env k
+    in (f hdl', act')
+
+------------------------------------------
+
+newtype ExecutorModeller m v x c a b s = ExecutorModeller {
+    runExecutorModeller :: RefExecutor m v s x c a b
+    }
+
+type instance F.Modeller (ExecutorModeller m v x c a b) s = RefExecutor m v s x c a b
+
+instance F.ViaModel (ExecutorModeller m v x c a b) where
+    viaModel l (Executor exec) = Executor $ \(ref, Lens this) k ->
+        let (hdl, act) = exec (ref, Lens (this.l)) k
+        in (F.viaModel l hdl, F.viaModel l act)

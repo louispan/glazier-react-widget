d0af8142462e1e396da886d10b108275d59b01c1
Louis Pan <louis@pan.me>
2018-02-10 00:00:08 +1100

    Obj derive generic

--- src/Glazier/React/Framework/Obj.hs
+++ src/Glazier/React/Framework/Obj.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -5,9 +6,12 @@ module Glazier.React.Framework.Obj where
 
 import Control.Lens
 import Data.IORef
+import qualified GHC.Generics as G
 
 -- | Uses ReifiedLens' to avoid impredicative polymorphism
-data Obj v s = Obj {ref :: IORef v, its :: ReifiedLens' v s }
+data Obj v s = Obj
+    { ref :: IORef v
+    , its :: ReifiedLens' v s } deriving (G.Generic)
 
 edit :: Lens' s a -> Obj v s -> Obj v a
 edit l obj = let Lens t = its obj in obj { its = Lens (t.l) }

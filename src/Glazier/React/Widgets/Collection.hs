9efe52d5a6a5abc13a89ceb52f0e570f58325b62
Louis Pan <louis@pan.me>
2018-12-24 21:19:39 +1100

    Moved UKey to separate file

--- src/Glazier/React/Widgets/Collection.hs
+++ src/Glazier/React/Widgets/Collection.hs
@@ -1,108 +1,12 @@
--- {-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
--- {-# LANGUAGE FlexibleInstances #-}
--- {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE RankNTypes #-}
--- {-# LANGUAGE RecordWildCards #-}
--- {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeFamilies #-}
 
-module Glazier.React.Widgets.Collection
-    ( UKey
-    , zeroUKey
-    , smallerUKey
-    , largerUKey
-    , betweenUKey
-    -- * Collection
-    , collectionWindow
-    ) where
+module Glazier.React.Widgets.Collection where
 
 import Control.Lens
 import Data.Foldable
-import qualified GHC.Generics as G
 import Glazier.React
-import qualified JavaScript.Extras as JE
-
--- | A key where you can always create
--- another key ordered between two different keys,
--- or another key above or below this key.
--- Memonic: U for uncountable https://en.wikipedia.org/wiki/Uncountable_set
-newtype UKey = UKey { unUKey :: [Int] }
-    deriving (G.Generic, Show)
-
--- | For comparison purposes, an empty list is equivalent to [0,0,..]
-instance Ord UKey where
-    compare (UKey []) (UKey []) = EQ
-    compare (UKey xs) (UKey []) = compare (UKey xs) (UKey [0])
-    compare (UKey []) (UKey ys) = compare (UKey [0]) (UKey ys)
-    compare (UKey (x : xs)) (UKey (y : ys)) = case compare x y of
-        EQ -> compare (UKey xs) (UKey ys)
-        o -> o
-
--- | For comparison purposes, an empty list is equivalent to [0,0,..]
-instance Eq UKey where
-    (UKey []) == (UKey []) = True
-    (UKey xs) == (UKey []) = (UKey xs) == (UKey [0])
-    (UKey []) == (UKey ys) = (UKey [0]) == (UKey ys)
-    (UKey (x : xs)) == (UKey (y : ys)) = if x == y
-        then (UKey xs) == (UKey ys)
-        else False
-
-zeroUKey :: UKey
-zeroUKey = UKey []
-
-ukeyStep :: Int
-ukeyStep = 32
-
--- | Create a key smaller than the input key.
-smallerUKey :: UKey -> UKey
-smallerUKey (UKey []) = UKey [-ukeyStep]
-smallerUKey (UKey (a : as)) = UKey $ case compare a (JE.minSafeInteger + ukeyStep) of
-        LT -> if JE.minSafeInteger == a
-            then JE.minSafeInteger : unUKey (smallerUKey (UKey as))
-            else [JE.minSafeInteger]
-        _ -> [a - ukeyStep]
-
--- | Create a key larger than the input key.
-largerUKey :: UKey -> UKey
-largerUKey (UKey []) = UKey [ukeyStep]
-largerUKey (UKey (a : as)) = UKey $ case compare a (JE.maxSafeInteger - ukeyStep) of
-        GT -> if JE.maxSafeInteger == a
-            then JE.maxSafeInteger : unUKey (largerUKey (UKey as))
-            else [JE.maxSafeInteger]
-        _ -> [a + ukeyStep]
-
--- | Make a key that will fit in between the two provided keys,
--- with no guarantees on how close it is to the mid point.
--- Except when the inputs are equal, then it will return the same key.
-betweenUKey :: UKey -> UKey -> UKey
-betweenUKey (UKey []) (UKey []) = zeroUKey
-betweenUKey (UKey xs) (UKey []) = betweenUKey (UKey xs) (UKey [0])
-betweenUKey (UKey []) (UKey ys) = betweenUKey (UKey [0]) (UKey ys)
-betweenUKey (UKey (x : xs)) (UKey (y : ys)) = UKey $ case compare x y of
-    LT -> if x + 1 == y
-        then x : unUKey (betweenUKey (UKey xs) (UKey $ repeat JE.maxSafeInteger))
-        else [betweenUncInt x y]
-    GT -> if y + 1 == x
-        then y : unUKey (betweenUKey (UKey ys) (UKey $ repeat JE.maxSafeInteger))
-        else [betweenUncInt x y]
-    EQ -> x : unUKey (betweenUKey (UKey xs) (UKey ys))
-
-betweenUncInt :: Int -> Int -> Int
-betweenUncInt x y =
-    let (xq, xr) = quotRem x 2
-        (yq, yr) = quotRem y 2
-        z = case (xr + yr) of
-            2 -> 1
-            _ -> 0
-    in xq + yq + z
-
------------------------------------------------------------------
 
 collectionWindow :: (Functor t, Foldable t)
     => ReactId -> Window (t (Obj s)) ()
